70536cc4b0f8eb9eb171089a07b85ece07a1069c
Hendrik Marx <h.marx@mytaxi.com>
2019-03-24 22:57:00 +0100

    remove empty implementations of lifecycle methods, add CallSuper Annotation to onStop() inline addDisposable()

--- app/src/main/kotlin/de/marxhendrik/healthcheckcards/base/LifecycleAwarePresenter.kt
+++ app/src/main/kotlin/de/marxhendrik/healthcheckcards/base/LifecycleAwarePresenter.kt
@@ -2,6 +2,7 @@ package de.marxhendrik.healthcheckcards.base
 
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.Lifecycle.Event.ON_STOP
+import android.support.annotation.CallSuper
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
@@ -9,25 +10,16 @@ abstract class LifecycleAwarePresenter : LifecycleAware {
 
     private val onStopDisposable = CompositeDisposable()
 
-    fun addDisposable(disposable: Disposable) = onStopDisposable.add(disposable)
-
     fun Disposable.manage(event: Lifecycle.Event = ON_STOP) {
         when (event) {
-            ON_STOP -> addDisposable(this)
+            ON_STOP -> onStopDisposable.add(this)
             else -> throw NotImplementedError("only ON_STOP is managed")
         }
     }
 
-    override fun onAttach() {
-    }
-
-    override fun onStart() {
-    }
-
+    @CallSuper
     override fun onStop() {
         onStopDisposable.clear()
     }
 
-    override fun onDetach() {
-    }
 }
